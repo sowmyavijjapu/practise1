cbe137c0b3488a89c78311d421d9c64d9f3918db
sowmyavijjapu <87028325+sowmyavijjapu@users.noreply.github.com>
2021-12-02 17:11:53 -0500

    Update App.java

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World -poll scm" );
+        System.out.println( "Hello World -github hook" );
     }
 }
